ad16d7b091f577c21495b50ba59a0b96e8997338
Joel <joel.eldoe2000@gmail.com>
2021-11-30 02:15:19 +0530

    Minor changes to the GUI

--- project_gui.py
+++ project_gui.py
@@ -7,18 +7,12 @@ ARNAV ANAND
 VIDYA LAD
 '''
 
-from tkinter import *
-from tkinter import messagebox
+from tkinter import *                   # Importing tkinter module to create a GUI application
+from tkinter import messagebox          # Importing messagebox from tkinter to show notifications
+import tkinter.font as tkFont           # Importing tkinter font
 import xor_file_encryptor               # Importing the module defined for XOR cipher
 import aes_file_encryptor               # Importing the module defined for AES cipher
 import os
-
-# ANSI codes for font colors
-class colors:
-    GREEN = '\033[92m'  # GREEN COLOR
-    YELLOW = '\033[93m' # YELLOW COLOR
-    RED = '\033[91m'    # RED COLOR
-    RESET = '\033[0m'   # RESET COLOR
 
 class File_Encryptor:
     # Validates the inputs, checks for file existence and password 
@@ -27,10 +21,8 @@ class File_Encryptor:
         if(len(password) >= 8 and len(password) <= 255):
             if not(os.path.isfile(filename)):
                 flag = False
-                print("This file doesn't exist in this directory!")
                 messagebox.showerror("ERROR", "This file doesn't exist in this directory!")
         else:
-            print(f"\n[{colors.RED}-{colors.RESET}] Error Occurred: Password length should be between 8-255!\n")
             flag = False
             messagebox.showwarning("WARNING", "Password length should be between 8-255!")
 
@@ -66,11 +58,9 @@ class File_Encryptor:
                 f.write(aes_encrypted_file_bytes)
                 f.close()
 
-                print(f"[{colors.GREEN}+{colors.RESET}] '{colors.GREEN}{filename+'.enc'}{colors.RESET}': encrypted file has been created successfully!")
                 messagebox.showinfo("RESULT", f"'{filename+'.enc'}': encrypted file has been created successfully!")
 
             except Exception as e:
-                print(f"Error: {e}")
                 messagebox.showerror("ERROR", f"{e}")
 
     # Implements file decryption
@@ -93,52 +83,60 @@ class File_Encryptor:
                 f.write(xor_decrypted_file_bytes.encode())
                 f.close()
 
-                print(f"[{colors.GREEN}+{colors.RESET}] '{colors.GREEN}{filename[:-4]+'.dec'}{colors.RESET}': decrypted file has been created successfully!")
                 messagebox.showinfo("RESULT", f"'{filename[:-4]+'.dec'}': decrypted file has been created successfully!")
 
             except Exception as e:
-                print(f"Error: {e}")
                 messagebox.showerror("ERROR", f"{e}")
 
 # Main program
-print("-----------------------------------------------------------------------")
-print(f"------------------------- {colors.GREEN}FILE ENCRYPTOR TOOL{colors.RESET} -------------------------")
-print("-----------------------------------------------------------------------\n")
-
-# GUI
 fe = File_Encryptor()
 
+# GUI
 app = Tk()
 app.geometry("500x500")
 app.title("FILE ENCRYPTOR")
 
+# Welcome message
 messagebox.showinfo("WELCOME", "Keep your data safe with us!")
 
+# Defining font
+font_style = tkFont.Font(family="Verdana", size="12")
+
+# Heading section
 heading = Label(app,text="DATA SECURITY USING FILE ENCRYPTION",fg="black",bg="powderblue",width="500",height="2",font="Helvetica 14 bold")
 heading.pack()
 
-filename_text = Label(app,text="FILE NAME :")
-password_text = Label(app,text="PASSWORD :")
-filename_text.place(x=15,y=70)
-password_text.place(x=15,y=140)
+# Body section
+# Defining labels
+filename_text = Label(app,text="FILE NAME :", font=font_style)
+password_text = Label(app,text="PASSWORD :", font=font_style)
+filename_text.place(x=100,y=70)
+password_text.place(x=100,y=150)
 
-file_name_entry = Entry(app,width="30")
-password_entry = Entry(app,width="30")
-file_name_entry.place(x=15,y=100)
-password_entry.place(x=15,y=180)
+# Defining the text entry
+file_name_entry = Entry(app, width="30", font=font_style)
+password_entry = Entry(app, width="30", font=font_style)
+file_name_entry.place(x=100,y=100, height="35")
+password_entry.place(x=100,y=180, height="35")
 
-button1 = Button(app,text="ENCRYPT FILE",command=fe.encrypt,width="30",height="2",bg="WHITE")
-button1.place(x=15,y=250)
+# Defining the buttons
+button1 = Button(app,text="ENCRYPT FILE",command=fe.encrypt,width="30",height="2",bg="WHITE", font=font_style)
+button1.place(x=100,y=250)
 
-button2 = Button(app,text="DECRYPT FILE",command=fe.decrypt,width="30",height="2",bg="WHITE")
-button2.place(x=15,y=300)
+button2 = Button(app,text="DECRYPT FILE",command=fe.decrypt,width="30",height="2",bg="WHITE", font=font_style)
+button2.place(x=100,y=310)
 
-button3 = Button(app,text="EXIT",command=app.quit,width="30",height="2",bg="WHITE")
-button3.place(x=15,y=350)
+button3 = Button(app,text="EXIT",command=app.quit,width="30",height="2",bg="WHITE", font=font_style)
+button3.place(x=100,y=370)
 
+# Running the app
 app.mainloop()
 
+# Deleting temporary files
 if(os.path.isfile('./temp.txt')):
     os.system('rm ./temp.txt')
 
+# Exit message
 messagebox.showinfo("BYE BYE", "Have a good day!")
+
+# End of program
